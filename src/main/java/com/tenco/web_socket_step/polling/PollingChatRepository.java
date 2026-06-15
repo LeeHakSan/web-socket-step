96202a32fe558b87b0900c14bb22eb49d093cdc6
san <san1425865@gamil.com>
2026-06-15 10:45:02 +0900

    feat:polling

---
+++ src/main/java/com/tenco/web_socket_step/polling/PollingChatRepository.java
@@ -0,0 +1,7 @@
+package com.tenco.web_socket_step.polling;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PollingChatRepository extends JpaRepository<PollingChat, Long> {
+
+}
